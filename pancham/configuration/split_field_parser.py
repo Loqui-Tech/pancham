343c8fa23becad591c6c8fd2b23df377bc910f3c
Andrew Tarry <andrew.tarry@gmail.com>
2025-02-13 12:25:24 +0200

    add handler for non string outcome

--- pancham/configuration/split_field_parser.py
+++ pancham/configuration/split_field_parser.py
@@ -35,6 +35,9 @@ class SplitFieldParser(FieldParser):
             if remove_pattern is not None and type(value) is str:
                 value = re.sub(remove_pattern, "", value)
 
+            if type(value) is not str:
+                return []
+
             splits = value.split(split_char)
             return [x.strip() for x in splits if len(x.strip()) > 0]
 
