74881d93fd032c000f0ff56071b2e765f58263c6
Andrew Tarry <andrew.tarry@gmail.com>
2025-02-28 09:53:29 +0200

    add cli test

--- test/pancham/test_data_frame_field.py
+++ test/pancham/test_data_frame_field.py
@@ -10,3 +10,11 @@ class TestDataFrameField():
         field = DataFrameField('a', 'b', str, func = lambda x: x)
 
         assert field.is_dynamic() == True
+
+    def test_str(self):
+        field = DataFrameField('a', 'b', str)
+
+        str_field = str(field)
+        expected = "Name: a, Source Name: b, Type: <class 'str'>, Nullable: True"
+
+        assert str_field == expected
