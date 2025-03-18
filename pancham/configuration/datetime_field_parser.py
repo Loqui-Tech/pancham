fffb9ec94f94146a2d405ee2a0dda9e660edc3d4
Andrew Tarry <andrew.tarry@gmail.com>
2025-03-18 21:14:29 +0200

    add date error

--- pancham/configuration/datetime_field_parser.py
+++ pancham/configuration/datetime_field_parser.py
@@ -1,5 +1,7 @@
 import datetime
 import pandas as pd
+from typing_extensions import Literal
+
 from .field_parser import FieldParser
 from pancham.data_frame_field import DataFrameField
 
@@ -16,10 +18,14 @@ class DateTimeFieldParser(FieldParser):
         if type(field[self.FUNCTION_KEY][self.FUNCTION_ID]) is dict:
             format = field[self.FUNCTION_KEY][self.FUNCTION_ID].get('format', '%d/%m/%Y')
 
+        on_error: Literal['coerce', 'ignore', 'raise'] = field.get('on_error', 'raise')
+        if on_error not in ['coerce', 'ignore', 'raise']:
+            on_error = 'raise'
+
         return DataFrameField(
             name = field['name'],
             field_type=datetime.datetime,
             nullable=self.is_nullable(field),
             source_name=None,
-            func=lambda x: pd.to_datetime(x[self.get_source_name(field)], format=format)
+            func=lambda x: pd.to_datetime(x[self.get_source_name(field)], format=format, errors=on_error)
         )
