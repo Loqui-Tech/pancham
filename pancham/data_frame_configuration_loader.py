fea2de697e0ba4e63b07f7db2b96df8476ea78e5
Andrew Tarry <andrew.tarry@gmail.com>
2025-04-22 13:45:24 +0300

    add iterator configuration

--- pancham/data_frame_configuration_loader.py
+++ pancham/data_frame_configuration_loader.py
@@ -23,7 +23,7 @@ class DataFrameConfigurationLoader:
 
         if data["file_type"] == "xlsx" and "sheet" in data:
             sheet = data["sheet"]
-
+            
         if (data['file_type'] == 'yaml' or data['file_type'] == 'json') and 'key' in data:
             key = data['key']
 
@@ -39,6 +39,9 @@ class DataFrameConfigurationLoader:
         if "validation" in data:
             for v in data['validation']:
                 configuration.validation_rules.append(self.__load_validation_configuration(v))
+
+        if data['use_iterator'] is True:
+            configuration.use_iterator = True
 
         return configuration
 
